f797606a284a33bdf3946b00b06e0d152b6c34fa
Jeff Verkoeyen <featherless@google.com>
2016-12-12 12:22:09 -0500

    Add write operator.
    Summary: Closes https://github.com/material-motion/streams-swift/issues/5
    
    Reviewers: markwei, O2 Material Motion, O4 Material Apple platform reviewers, #material_motion
    
    Reviewed By: markwei, O2 Material Motion, O4 Material Apple platform reviewers, #material_motion
    
    Subscribers: markwei
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2222

---
+++ src/operators/write.swift
@@ -0,0 +1,36 @@
+/*
+ Copyright 2016-present The Material Motion Authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import Foundation
+
+extension MotionObservable {
+
+  /** Write incoming values to the provided property. */
+  public func write<S: ScopedWriteable>(to property: S) -> MotionObservable<T> where S.V == T {
+    return _operator { observer, value in
+      property.write(value)
+      observer.next(value)
+    }
+  }
+
+  /** Write incoming values using the provided inline block. */
+  public func write(to inline: @escaping (T) -> Void) -> MotionObservable<T> {
+    return _operator { observer, value in
+      inline(value)
+      observer.next(value)
+    }
+  }
+}
