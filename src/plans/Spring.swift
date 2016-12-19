b4ec2bebb21aec6b9ffc34f94dcc0429ec0a8ab7
Jeff Verkoeyen <featherless@google.com>
2016-12-19 14:59:35 -0500

    Add Spring plan.
    Summary: Spec: https://material-motion.github.io/material-motion/starmap/specifications/streams/plans/Spring
    
    Reviewers: O2 Material Motion, chuga, O4 Material Apple platform reviewers, #material_motion
    
    Reviewed By: chuga, O4 Material Apple platform reviewers
    
    Subscribers: chuga
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2357

---
+++ src/plans/Spring.swift
@@ -0,0 +1,104 @@
+/*
+ Copyright 2016-present The Material Motion Authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import Foundation
+
+/**
+ A Spring can pull a value from an initial position to a destination using a physical simulation.
+
+ This class defines the expected shape of a Spring for use in creating a Spring source.
+ */
+public final class Spring<T: Zeroable> {
+
+  /** Creates a spring with the provided properties and an initial velocity of zero. */
+  public convenience init(to destination: ReactiveProperty<T>, initialValue: ReactiveProperty<T>, threshold: CGFloat) {
+    var velocity: T = T.zero() as! T
+    let initialVelocity = ReactiveProperty<T>(read: { velocity }, write: { velocity = $0 })
+    self.init(to: destination, initialValue: initialValue, initialVelocity: initialVelocity, threshold: threshold)
+  }
+
+  /** Creates a spring with the provided properties and an initial velocity. */
+  public init(to destination: ReactiveProperty<T>,
+              initialValue: ReactiveProperty<T>,
+              initialVelocity: ReactiveProperty<T>,
+              threshold: CGFloat) {
+    self.destination = destination
+    self.initialValue = initialValue
+    self.initialVelocity = initialVelocity
+
+    var threshold = threshold
+    self.threshold = ReactiveProperty<CGFloat>(read: { threshold }, write: { threshold = $0 })
+
+    var configuration = SpringConfiguration.defaultConfiguration
+    self.configuration = ReactiveProperty<SpringConfiguration>(read: { configuration },
+                                                             write: { configuration = $0 })
+  }
+
+  /** The destination value of the spring represented as a property. */
+  public let destination: ReactiveProperty<T>
+
+  /** The initial value of the spring represented as a property. */
+  public let initialValue: ReactiveProperty<T>
+
+  /** The initial velocity of the spring represented as a property. */
+  public let initialVelocity: ReactiveProperty<T>
+
+  /** The configuration of the spring represented as a property. */
+  public let configuration: ReactiveProperty<SpringConfiguration>
+
+  /** The value used when determining completion of the spring simulation. */
+  public let threshold: ReactiveProperty<CGFloat>
+}
+
+/**
+ Configure the spring traits for a given property.
+
+ Affects the spring behavior of the SpringTo plan.
+ */
+public final class SpringConfiguration {
+  /**
+   The tension coefficient for the property's spring.
+
+   If nil, the spring's tension will not be changed.
+   */
+  public var tension: CGFloat
+
+  /**
+   The friction coefficient for the property's spring.
+
+   If nil, the spring's friction will not be changed.
+   */
+  public var friction: CGFloat
+
+  /** Initializes the configuration with a given tension and friction. */
+  @objc(initWithTension:friction:)
+  public init(tension: CGFloat, friction: CGFloat) {
+    self.tension = tension
+    self.friction = friction
+  }
+
+  /**
+   The default spring configuration.
+
+   Default extracted from a POP spring with speed = 12 and bounciness = 4.
+   */
+  public static var defaultConfiguration: SpringConfiguration {
+    get {
+      // Always return a new instance so that the values can't be changed externally.
+      return SpringConfiguration(tension: 342, friction: 30)
+    }
+  }
+}
---
+++ tests/unit/plans/Spring.swift
@@ -0,0 +1,40 @@
+/*
+ Copyright 2016-present The Material Motion Authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import XCTest
+import CoreGraphics
+import MaterialMotionStreams
+
+class SpringTests: XCTestCase {
+
+  func testCGPointInitialization() {
+    let view = UIView()
+    let target = UIView()
+    let spring = Spring(to: propertyOf(target).center,
+                        initialValue: propertyOf(view).center,
+                        threshold: 1)
+    XCTAssertEqual(spring.initialVelocity.read(), .zero)
+  }
+
+  func testCGFloatInitialization() {
+    let view = UIView()
+    let target = UIView()
+    let spring = Spring(to: propertyOf(target).centerX,
+                        initialValue: propertyOf(view).centerX,
+                        threshold: 1)
+    XCTAssertEqual(spring.initialVelocity.read(), 0)
+  }
+}
