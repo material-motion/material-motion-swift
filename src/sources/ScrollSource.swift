90db9a66afcc40a45ed89f86fb33c6c7cf443b28
Jeff Verkoeyen <featherless@google.com>
2016-12-19 15:04:17 -0500

    Add ScrollSource.
    Reviewers: chuga, O4 Material Apple platform reviewers, O2 Material Motion, #material_motion
    
    Reviewed By: chuga, O4 Material Apple platform reviewers
    
    Subscribers: chuga
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2369

---
+++ src/sources/ScrollSource.swift
@@ -0,0 +1,72 @@
+/*
+ Copyright 2016-present The Material Motion Authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import UIKit
+
+/** A ScrollSource is a function that creates a MotionObservable from a UIScrollView. */
+public typealias ScrollSource = (UIScrollView) -> MotionObservable<CGPoint>
+
+public func scrollSource(_ scrollView: UIScrollView) -> MotionObservable<CGPoint> {
+  return MotionObservable { observer in
+    return ScrollViewConnection(subscribedTo: scrollView, observer: observer).disconnect
+  }
+}
+
+private var scrollViewConnectionContext = 0
+
+private final class ScrollViewConnection: NSObject {
+  deinit {
+    self.disconnect()
+  }
+
+  init(subscribedTo scrollView: UIScrollView, observer: MotionObserver<CGPoint>) {
+    self.scrollView = scrollView
+    self.observer = observer
+
+    super.init()
+
+    scrollView.addObserver(self,
+                           forKeyPath: NSStringFromSelector(#selector(getter: UIScrollView.contentOffset)),
+                           options: .new,
+                           context: &scrollViewConnectionContext)
+
+    observer.next(scrollView.contentOffset)
+  }
+
+  func disconnect() {
+    scrollView?.removeObserver(self,
+                               forKeyPath: NSStringFromSelector(#selector(getter: UIScrollView.contentOffset)),
+                               context: &scrollViewConnectionContext)
+    scrollView = nil
+  }
+
+  override func observeValue(forKeyPath keyPath: String?,
+                             of object: Any?,
+                             change: [NSKeyValueChangeKey : Any]?,
+                             context: UnsafeMutableRawPointer?) {
+    if context == &scrollViewConnectionContext {
+      if let newValue = change?[.newKey] as? CGPoint {
+        observer.next(newValue)
+      }
+
+    } else {
+      super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+    }
+  }
+
+  private var scrollView: UIScrollView?
+  private let observer: MotionObserver<CGPoint>
+}
