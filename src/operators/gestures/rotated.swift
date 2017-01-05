ce8bdea7684fe5e90aae6a5064a98453dc627a45
Jeff Verkoeyen <featherless@google.com>
2017-01-05 16:17:26 -0500

    Add rotated operator.
    Reviewers: markwei, O2 Material Motion, O4 Material Apple platform reviewers, #material_motion
    
    Reviewed By: markwei, O2 Material Motion, O4 Material Apple platform reviewers, #material_motion
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2460

---
+++ src/operators/gestures/rotated.swift
@@ -0,0 +1,39 @@
+/*
+ Copyright 2016-present The Material Motion Authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import Foundation
+
+extension ExtendableMotionObservable where T: UIRotationGestureRecognizer {
+
+  /**
+   Adds the current translation to the initial position and emits the result while the gesture
+   recognizer is active.
+   */
+  func rotated<P: Readable>(from initialRotation: P) -> MotionObservable<CGFloat> where P.T == CGFloat {
+    var cachedInitialRotation: CGFloat?
+    return _nextOperator { value, next in
+      if value.state == .began || (value.state == .changed && cachedInitialRotation == nil)  {
+        cachedInitialRotation = initialRotation.read()
+      } else if value.state != .began && value.state != .changed {
+        cachedInitialRotation = nil
+      }
+      if let cachedInitialRotation = cachedInitialRotation {
+        let rotation = value.rotation
+        next(cachedInitialRotation + rotation)
+      }
+    }
+  }
+}
