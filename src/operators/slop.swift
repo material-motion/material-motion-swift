6fb590f45c98d27370c89491c05267ec7da9000b
Jeff Verkoeyen <featherless@google.com>
2017-02-01 11:38:08 -0500

    Add slop operator.
    Summary:
    Example usage:
    
    ```
    runtime.add(runtime.get(pan)
      .translation(in: runtime.containerView)
      .y()
      .slop(size: 50, onExit: .backward, onReturn: .forward),
                to: transition.direction)
    ```
    
    Reviewers: O2 Material Motion, O4 Material Apple platform reviewers, #material_motion, chuga
    
    Reviewed By: O4 Material Apple platform reviewers, chuga
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2639

---
+++ src/operators/slop.swift
@@ -0,0 +1,51 @@
+/*
+ Copyright 2016-present The Material Motion Authors. All Rights Reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import Foundation
+
+extension MotionObservableConvertible where T == CGFloat {
+
+  /**
+   Emits values in reaction to exiting a slop region.
+
+   The slop region is centered around 0 and has the given size. This operator will not emit any
+   values until the upstream value exits this slop region, at which point the onExit value will be
+   emitted. If the upstream returns to the slop region then onReturn will be emitted.
+   */
+  public func slop<U: Equatable>(size: CGFloat, onExit: U?, onReturn: U?) -> MotionObservable<U> {
+    let didLeaveSlopRegion = createProperty(withInitialValue: false)
+
+    return MotionObservable { observer in
+      let upstreamSubscription = self
+        .threshold(min: -size, max: size,
+                   whenBelow: true, whenWithin: nil, whenAbove: true)
+        .dedupe()
+        .subscribe(next: { didLeaveSlopRegion.value = $0 }, state: { _ in }, coreAnimation: { _ in })
+
+      let downstreamSubscription = self
+        .valve(openWhenTrue: didLeaveSlopRegion)
+        .threshold(min: -size, max: size,
+                   whenBelow: onExit, whenWithin: onReturn, whenAbove: onExit)
+        .dedupe()
+        .subscribe(next: observer.next, state: observer.state, coreAnimation: observer.coreAnimation)
+
+      return {
+        upstreamSubscription.unsubscribe()
+        downstreamSubscription.unsubscribe()
+      }
+    }
+  }
+}
