439452c99ecd35242c886b5a27f6a003d39c6519
Jeff Verkoeyen <featherless@google.com>
2017-02-01 11:36:59 -0500

    Make translated a public operator on reactive UIPanGestureRecognizer.
    Reviewers: O2 Material Motion, O4 Material Apple platform reviewers, #material_motion, chuga
    
    Reviewed By: O4 Material Apple platform reviewers, chuga
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D2630

--- src/reactivetypes/ReactiveUIGestureRecognizer.swift
+++ src/reactivetypes/ReactiveUIGestureRecognizer.swift
@@ -49,10 +49,9 @@ extension ReactiveUIGestureRecognizer {
 
 extension ReactiveUIGestureRecognizer where O: UIPanGestureRecognizer {
 
-  func translated<O: MotionObservableConvertible>(from initialPosition: O) -> MotionObservable<CGPoint> where O.T == CGPoint {
-    return asStream().translated(from: initialPosition, in: containerView)
+  public func translated<O: MotionObservableConvertible>(from initialPosition: O) -> MotionObservable<CGPoint> where O.T == CGPoint {
+    return asStream().onRecognitionStates([.began, .changed]).translated(from: initialPosition, in: containerView)
   }
-
   public func velocityOnReleaseStream() -> MotionObservable<CGPoint> {
     return gestureToStream(gestureRecognizer).onRecognitionState(.ended).velocity(in: containerView)
   }
