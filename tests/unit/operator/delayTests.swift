67cb7b18dccce117e1a7d248e0919b4f9edb613d
Jeff Verkoeyen <featherless@google.com>
2017-04-27 13:30:08 -0400

    Shorten the delayBy test delay.
    Summary: Speeding up the tests.
    
    Reviewers: O2 Material Motion, O4 Material Apple platform reviewers, #material_motion, appsforartists
    
    Reviewed By: O2 Material Motion, #material_motion, appsforartists
    
    Tags: #material_motion
    
    Differential Revision: http://codereview.cc/D3125

--- tests/unit/operator/delayTests.swift
+++ tests/unit/operator/delayTests.swift
@@ -42,7 +42,7 @@ class delayTests: XCTestCase {
 
     var hasReceived = false
     let didReceiveValue = expectation(description: "Did receive value")
-    let subscription = property.delay(by: .milliseconds(300)).subscribeToValue { value in
+    let subscription = property.delay(by: .milliseconds(10)).subscribeToValue { value in
       XCTAssertEqual(value, 0)
       didReceiveValue.fulfill()
       hasReceived = true
@@ -50,7 +50,7 @@ class delayTests: XCTestCase {
 
     XCTAssertFalse(hasReceived)
 
-    waitForExpectations(timeout: 0.5)
+    waitForExpectations(timeout: 0.1)
 
     subscription.unsubscribe()
   }
