82b804b493af821164b90983d46f0520bfd3c208
Jeff Verkoeyen <featherless@google.com>
2017-05-05 13:21:59 -0400

    Set position on tap.

--- examples/SetPositionOnTapExample.swift
+++ examples/SetPositionOnTapExample.swift
@@ -18,10 +18,56 @@ import UIKit
 import IndefiniteObservable
 import MaterialMotion
 
-public func positionOnTap(inCoordinateSpace coordinateSpace: UIView, withGestureRecognizer gestureRecognizer: UITapGestureRecognizer) -> MotionObservable<CGPoint> {
-  let gestureTarget = ReactiveGestureTarget()
-  gestureRecognizer.addTarget(gestureTarget, action: #selector(ReactiveGestureTarget.gestureDidChange))
-  return gestureTarget.didChange.centroid(in: coordinateSpace)
+public final class SetPositionOnTap2: Interaction2, Stateful {
+  init(_ target: ReactiveProperty<CGPoint>, containerView: UIView, withGestureRecognizer existingGesture: UITapGestureRecognizer) {
+    self.target = target
+    self.containerView = containerView
+    self.gestureRecognizer = existingGesture
+  }
+
+  public func enable() {
+    guard subscriptions.isEmpty else { return }
+
+    let gestureRecognizer: UITapGestureRecognizer
+    if let existingGestureRecognizer = self.gestureRecognizer {
+      gestureRecognizer = existingGestureRecognizer
+    } else {
+      gestureRecognizer = UITapGestureRecognizer()
+      self.containerView.addGestureRecognizer(gestureRecognizer)
+    }
+
+    let target = self.target
+    let containerView = self.containerView
+    let state = _state
+
+    subscriptions.append(contentsOf: [
+      Reactive(gestureRecognizer).didRecognize.subscribeToValue { [weak containerView] in
+        guard let containerView = containerView else { return }
+        target.value = $0.location(in: containerView)
+      },
+      Reactive(gestureRecognizer).state.subscribeToValue {
+        state.value = $0
+      }
+      ]
+    )
+  }
+
+  public func disable() {
+    subscriptions.forEach { $0.unsubscribe() }
+    subscriptions.removeAll()
+    _state.value = .atRest
+  }
+
+  public var state: MotionObservable<MotionState> {
+    return _state.asStream()
+  }
+  private let _state = createProperty(withInitialValue: MotionState.atRest)
+
+  private let target: ReactiveProperty<CGPoint>
+  private let containerView: UIView
+  private var subscriptions: [Subscription] = []
+
+  public var gestureRecognizer: UITapGestureRecognizer?
 }
 
 class SetPositionOnTapExampleViewController: ExampleViewController {
@@ -35,11 +81,12 @@ class SetPositionOnTapExampleViewController: ExampleViewController {
     let tap = UITapGestureRecognizer()
     view.addGestureRecognizer(tap)
 
-    Reactive(tap).events.centroid(in: view).subscribeToValue {
-      square.center = $0
-    }
+    let setPosition = SetPositionOnTap2(Reactive(square.layer).position,
+                                        containerView: view,
+                                        withGestureRecognizer: tap)
+    setPosition.enable()
 
-    Reactive(tap).state.subscribeToValue { state in
+    setPosition.state.subscribeToValue { state in
       print(state)
     }
   }
